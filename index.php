7bc5aaf205274e4f1aae84cd54a5b80dd55743d8
root <root@kayenkhedma.com>
2017-11-25 13:39:10 +0000

    Bumped asset versions

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include('mail/mail.php');
   <script src="https://s.codepen.io/assets/libs/modernizr.js" type="text/javascript"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/style.css?v=6.9">
 <link rel="icon"
      href="logo.png"/>
 </head>
